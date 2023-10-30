60629539c2148c6732e4ccbffe4a2612761f002a
Pain31415 <Cherniy412@ukr.net>
2023-10-30 18:54:50 +0200

    Add project files.

---
+++ func.cpp
@@ -0,0 +1,22 @@
+#include "func.h"
+#include <iostream>
+
+using namespace std;
+
+void printRectangle(int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            cout << "* ";
+        }
+        cout << :endl;
+    }
+}
+void showTriangle() {
+
+}
+void showCircle() {
+
+}
+void showSquare() {
+
+}
---
+++ func.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printRectangle(int height, int width);
+void showTriangle();
+void showCircle();
+void showSquare();
